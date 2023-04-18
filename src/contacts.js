4188c21f3af9559b27ddf7075d703b42d6582a97
Pavan Kumar <pavanrnri1818@gmail.com>
2023-04-18 21:44:11 +0530

    learn about react component and this my final commit

---
+++ src/contacts.js
@@ -0,0 +1,46 @@
+const contacts = [
+    {
+        id:1,
+        name:"Beyonce",
+        imgURL:"https://images.pexels.com/photos/789822/pexels-photo-789822.jpeg?auto=compress&cs=tinysrgb&w=600",
+        phone:"+123 456 789",
+        email:"b@beyonce.com"
+    },
+    {
+        id:2,
+        name:"Ivan",
+        imgURL:"https://images.pexels.com/photos/190168/pexels-photo-190168.jpeg?auto=compress&cs=tinysrgb&w=600",
+        phone:"+123 789 456",
+        email:"b@ivan.com"
+    },
+    {
+        id:3,
+        name:"Andrea",
+        imgURL:"https://images.pexels.com/photos/879143/pexels-photo-879143.jpeg?auto=compress&cs=tinysrgb&w=600",
+        phone:"+123 987 456",
+        email:"b@andrea.com"
+    },
+    {
+        id:4,
+        name:"Anastasiya",
+        imgURL:"https://images.pexels.com/photos/1462630/pexels-photo-1462630.jpeg?auto=compress&cs=tinysrgb&w=600",
+        phone:"+123 654 987",
+        email:"b@bnastasiya.com"
+    },
+    {
+        id:5,
+        name:"Anastasiya",
+        imgURL:"https://images.pexels.com/photos/1462630/pexels-photo-1462630.jpeg?auto=compress&cs=tinysrgb&w=600",
+        phone:"+123 654 987",
+        email:"b@bnastasiya.com"
+    },
+    {
+        id:6,
+        name:"Anastasiya",
+        imgURL:"https://images.pexels.com/photos/1462630/pexels-photo-1462630.jpeg?auto=compress&cs=tinysrgb&w=600",
+        phone:"+123 654 987",
+        email:"b@bnastasiya.com"
+    }
+
+];
+export default contacts;
